f492948b840406d63aa5ea624a946fce67928bf2
rteachey <ricky@teachey.org>
2024-01-05 15:17:01 -0500

    Working version

---
+++ aashto_plastic_pipe_check.py
@@ -0,0 +1,92 @@
+"""AASHTO 12.12.3- Thermoplastic Pipe Checks"""
+
+__version__ = "0.0.1"
+
+from typing import Literal, Callable
+import xlwings as xw
+from pint import UnitRegistry, Quantity, Unit
+from excalc_py import create_calculation, adapt_function
+
+# xlwings interop
+WB_NAME = "C:\\Users\\rteachey\\projects\\aashto_plastic_pipe_check\\AASHTO plastic pipe structural calcs.xlsx"
+APP = xw.App(visible=False)
+WB: xw.Book = APP.books.open(WB_NAME)
+SH: xw.Sheet = WB.sheets[0]
+OUTPUT_RNG = SH.range("CheckAll")
+
+# pint interop
+U = UnitRegistry()
+
+RNG_UNITS_DICT = dict[str, (str, Unit | None)](
+    pipe_type=("PipeType", None),
+    OD_nom=("OD_nom", U.inch),
+    H=("H", U.feet),
+    H_gw=("H_gw", U.feet),
+    γ_soil=("γ_soil", U.lbf / U.feet**3),
+    Ms=("Ms", U.psi),
+    soil_class=("SoilClass", None),
+    compaction=("Proctor", None),
+    proctor=("ProctorType", None),
+    grain=("Grain", None),
+    ν=("ν", None),
+    E_long=("E_long", U.psi),
+    E_short=("E_short", U.psi),
+    Fy_long=("Fy_long", U.psi),
+    Fy_short=("Fy_short", U.psi),
+)
+
+
+# adapt for units
+def _its_a_quantity(quantity: Quantity) -> Quantity:
+    return quantity
+
+
+_ADAPT_DICT: dict[None | UnitRegistry, Callable] = {
+    U: lambda quantity: quantity,
+    # a str can be turned into a quantity
+    None: lambda quantity_str: U.Quantity(quantity_str),
+}
+
+
+def _ensure_quantity(quantity: Quantity | str) -> Quantity:
+    # handle a quantity from another pint unit registry
+    registry = getattr(quantity, '_REGISTRY', None)
+    return _ADAPT_DICT.setdefault(registry, _its_a_quantity)(quantity)
+
+
+def _input_unit_adapter(unit: Unit) -> Callable[[Quantity | str], float]:
+    def input_adapter(quantity):
+        return _ensure_quantity(quantity).to(unit).magnitude
+    return input_adapter
+
+
+INPUT_RNG_DCT = {k: SH.range(v[0]) for k, v in RNG_UNITS_DICT.items()}
+INPUT_UNITS_DCT = {
+    k: _input_unit_adapter(v[1])
+    for k, v in RNG_UNITS_DICT.items()
+    if v[1] is not None
+}
+
+
+# noinspection PyUnusedLocal
+@adapt_function(**INPUT_UNITS_DCT)
+@create_calculation(OUTPUT_RNG, **INPUT_RNG_DCT)
+def check(
+    pipe_type: Literal["DR17", "DR32.5"],
+    OD_nom: Quantity,
+    H: Quantity,
+    H_gw: Quantity,
+    γ_soil: Quantity,
+    Ms: Quantity = None,
+    soil_class: Literal["I", "IA", "IB", "II", "III"] = None,
+    compaction: Literal["Compacted", "Uncompacted", "85%", "90%", "95%", "100%"] = None,
+    proctor: Literal["Standard Proctor", "Modified Proctor"] = None,
+    grain: Literal["Gravel", "Sand"] = None,
+    ν: float = 0.35,
+    E_long: Quantity = 22_000 * U.psi,
+    E_short: Quantity = 110_000 * U.psi,
+    Fy_long: Quantity = 1_440 * U.psi,
+    Fy_short: Quantity = 3_000 * U.psi,
+):
+    """Uses the AASHTO plastic pipe structural calcs.xlsx spreadsheet to perform structural checks based on inputs."""
+    ...
---
+++ test/test_check.py
@@ -0,0 +1,69 @@
+import pytest
+from aashto_plastic_pipe_check import check, U, APP
+from pint import UnitRegistry
+
+FOREIGN_REG = UnitRegistry()
+
+
+@pytest.fixture(autouse=True, scope="session")
+def xw_app():
+    yield APP
+    APP.quit()
+
+
+@pytest.fixture(
+    params=[
+        dict(
+            pipe_type=None,
+            OD_nom=None,
+            H=None,
+            H_gw=None,
+            γ_soil=None,
+            Ms=None,
+            soil_class=None,
+            compaction=None,
+            proctor=None,
+            grain=None,
+            ν=None,
+            E_long=None,
+            E_short=None,
+            Fy_long=None,
+            Fy_short=None,
+        ),
+        dict(
+            pipe_type=None,
+            OD_nom=None,
+            H=None,
+            H_gw=None,
+            γ_soil=None,
+        ),
+        dict(
+            pipe_type="32.5",
+            OD_nom=48*U.inch,
+            H=10*U.ft,
+            H_gw=10*U.ft,
+            γ_soil=120.0*U.lbf/U.ft**3,
+        ),
+        dict(
+            pipe_type="32.5",
+            OD_nom="48 * inch",
+            H="10 * ft",
+            H_gw="10 * ft",
+            γ_soil="120.0 * lbf / ft ** 3",
+        ),
+        dict(
+            pipe_type="32.5",
+            OD_nom=48 * FOREIGN_REG.inch,
+            H=10 * FOREIGN_REG.ft,
+            H_gw=10 * FOREIGN_REG.ft,
+            γ_soil=120.0 * FOREIGN_REG.lbf / FOREIGN_REG.ft ** 3,
+        ),
+    ], ids=("All None", "Partial None", "Partial w units", "Partial str units", "Foreign pint unit registry")
+)
+def check_kwargs(request):
+    return request.param
+
+
+def test_check(check_kwargs):
+    result = check(**check_kwargs)
+    assert result
